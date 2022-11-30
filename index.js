f172fbecf8ed5db22c6e837d949cee791455969c
Megan <dommelmeg@gmail.com>
2022-11-29 22:26:33 -0500

    added non-alcoholic drink render

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 const mainBody = document.getElementById('title')
 const cocktailCardDiv = document.getElementById('cocktailCardDiv')
 const headerBtns = document.getElementsByClassName('headerBtn')
+const sshLogo = document.getElementById('logo')
+const multiCardHolderDiv = document.getElementById('multiCardHolder')
 
 const getRandomCocktail = () => {
   fetch('https://www.thecocktaildb.com/api/json/v1/1/random.php')
@@ -8,6 +10,7 @@ const getRandomCocktail = () => {
     .then((drink) => renderOneDrink(drink))
 }
 
+//Need to add title text via JS (not hard coded)
 const renderOneDrink = (randomDrink) => {
   //*bonus* add event listener on 'shake it up' button – won't autogenerate when opening page
   const randomDrinkArray = randomDrink.drinks
@@ -28,50 +31,47 @@ const renderOneDrink = (randomDrink) => {
   document.getElementById('cocktailCardDiv').appendChild(card)
 }
 
+//Add click event to logo
+
 //Alcoholic Section
 const renderAlcoholicDrinks = (listOfDrinks) => {
   cocktailCardDiv.innerHTML = null
   cocktailCardDiv.className = 'multiCardHolder'
   nonAlcoholicText.style.backgroundColor = 'white'
   alcoholicText.style.backgroundColor = '#F2F2F2'
+
   const allDrinksArray = listOfDrinks.drinks
-  // console.log(allDrinksArray)
-  allDrinksArray.forEach((drink, index) => {
-    console.log(index)
+  allDrinksArray.forEach(drink => {
     const { strDrink, strDrinkThumb, idDrink } = drink
     let card = document.createElement('div')
     card.className = 'card'
     card.innerHTML = `
-      <img src="${strDrinkThumb}"
-        class='drinkImage'
-        alt="${strDrink}"
-        id='${strDrink} image'
-      />
-      <h2>${strDrink}</h2>
-      <br>
-      <button class=yesPleaseBtn>Yes, please!`
-    // card.innerHTML = `
-    // <img src="${strDrinkThumb}"
-    // class='drinkImage'
-    // alt="${strDrink}
-    // id='${strDrink} image'
-    // />
-    // <h2>${strDrink}</h2>
-    // <button class=imOverItBtn>I'm Over It!`
+    <img src="${strDrinkThumb}"
+    class='drinkImage'
+    alt="${strDrink}"
+    id='${strDrink} image'
+    />
+    <h2>${strDrink}</h2>
+    <br>
+    <button class=yesPleaseBtn>Yes, please!`
     document.getElementById('cocktailCardDiv').appendChild(card)
   })
 }
 
+const renderManyDrinks = (listOfDrinks) => {
+  
+}
+
 const getAlcoholicDrinks = () => {
   fetch('https://www.thecocktaildb.com/api/json/v1/1/filter.php?a=Alcoholic')
-    .then((res) => res.json())
-    .then((drinks) => renderAlcoholicDrinks(drinks))
+  .then((res) => res.json())
+  .then((drinks) => renderAlcoholicDrinks(drinks))
 }
 
 const getNonAlcoholicDrinks = () => {
   fetch('https://www.thecocktaildb.com/api/json/v1/1/filter.php?a=Non_Alcoholic')
-    .then((res) => res.json())
-    .then(drinks => renderNonAlcoholicDrinks(drinks))
+  .then((res) => res.json())
+  .then(drinks => renderNonAlcoholicDrinks(drinks))
 }
 
 const alcoholicText = document.getElementById('alcoholic')
@@ -84,20 +84,41 @@ nonAlcoholicText.addEventListener('click', getNonAlcoholicDrinks)
 getRandomCocktail()
 
 //Non-alcoholic section
-  //Able to filter by ingredient
+//Able to filter by ingredient
 const renderNonAlcoholicDrinks = (listOfDrinks) => {
-  mainBody.innerHTML = null
+  cocktailCardDiv.innerHTML = null
+  cocktailCardDiv.className = 'multiCardHolder'
   alcoholicText.style.backgroundColor = 'white'
   nonAlcoholicText.style.backgroundColor = '#F2F2F2'
-  console.log(listOfDrinks)
-  }
+
+  const allDrinksArray = listOfDrinks.drinks
+  allDrinksArray.forEach(drink => {
+    const { strDrink, strDrinkThumb, idDrink } = drink
+    let card = document.createElement('div')
+    card.className = 'card'
+    card.innerHTML = `
+    <img src="${strDrinkThumb}"
+    class='drinkImage'
+    alt="${strDrink}"
+    id='${strDrink} image'
+    />
+    <h2>${strDrink}</h2>
+    <br>
+    <button class=yesPleaseBtn>Yes, please!`
+    document.getElementById('cocktailCardDiv').appendChild(card)
+  })
+}
 
 //Search section
-  //Search based on a keyword
+//Search based on a keyword
 
 //Favorites List
-  //Add event listener to 'Yes, please' btn
-    //Adds cocktail info + instructions to favorites list (adds to a new array)
-    //Add button w/ event listener that deletes cocktail from favorites (deletes from new array)
+//Add event listener to 'Yes, please' btn
+//Adds cocktail info + instructions to favorites list (adds to a new array)
+//Add button w/ event listener that deletes cocktail from favorites (deletes from new array)
 
 
+sshLogo.addEventListener('click', function(){
+  console.log('hi')
+  getRandomCocktail()
+})
